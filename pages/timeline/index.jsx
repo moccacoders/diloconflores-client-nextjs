e362945225072048da97a7aa2cafedad645b74d8
Raymundo Salazar <alberto.salazar@globant.com>
2022-04-22 03:23:34 -0500

    test to use linter y prettier before commit

--- pages/timeline/index.jsx
+++ pages/timeline/index.jsx
@@ -3,35 +3,37 @@ import Button, { Link } from "atoms/Buttons"
 import { Title } from "atoms/Typography"
 
 export default function Timeline({ userName }) {
-    return (
-        <AppLayout>
-            <Title size="large">This is the Timeline of {userName}</Title>
-            <Button style="primary" size="sm">
-                Go Home
-            </Button>
-            <Button style="secondary">Go Home</Button>
-            <Button style="success" size="lg" disabled>
-                Go Home
-            </Button>
-            <Link href="/">Go Home</Link>
-            <Link href="https://google.com" target="_blank">
-                Go Google
-            </Link>
-            <Link
-                style="light"
-                button
-                href="https://google.com"
-                disabled
-                target="_blank"
-            >
-                Go Google
-            </Link>
-        </AppLayout>
-    )
+	const a = "hello"
+
+	return (
+		<AppLayout>
+			<Title size="large">This is the Timeline of {userName}</Title>
+			<Button style="primary" size="sm">
+				Go Home
+			</Button>
+			<Button style="secondary">Go Home</Button>
+			<Button style="success" size="lg" disabled>
+				Go Home
+			</Button>
+			<Link href="/">Go Home</Link>
+			<Link href="https://google.com" target="_blank">
+				Go Google
+			</Link>
+			<Link
+				style="light"
+				button
+				href="https://google.com"
+				disabled
+				target="_blank"
+			>
+				Go Google
+			</Link>
+		</AppLayout>
+	)
 }
 
 Timeline.getInitialProps = () => {
-    return fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + "hello").then((res) =>
-        res.json()
-    )
+	return fetch(process.env.NEXT_PUBLIC_API_ENDPOINT + "hello").then((res) =>
+		res.json()
+	)
 }
